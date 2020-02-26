b3e76b61e0fe90ee5e236f600b62a10299ee89e9
rob <robertahuber88@gmail.com>
2020-02-26 17:26:06 +0100

    commit styles

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,122 +1,65 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 class HomePage extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <>
-
-            <div id="cardCont">
-              <div className="card-1">
-                <a
-                  href="https://design.tutsplus.com/articles/international-artist-feature-malaysia--cms-26852"
-                  className="card"
-                >
-                  <a
-                    className="thumb"
-                    href="https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/flex-1.jpg"
-                  />
-                  <article>
-                    <h1>International Artist Feature: Malaysia</h1>
-                    <span>Mary Winkler</span>
-                  </article>
-                </a>
-              </div>
-              <div class="card-1">
-                <a
-                  href="https://webdesign.tutsplus.com/articles/how-to-conduct-remote-usability-testing--cms-27045"
-                  className="card"
-                >
-                  <div
-                    className="thumb"
-                    href="https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/flex-1.jpg"
-                  />
-                  <article>
-                    <h1>How to Conduct Remote Usability Testing</h1>
-                    <span>Harry Brignull</span>
-                  </article>
-                </a>
-              </div>
-              <div className="card-1">
-                <a
-                  href="https://design.tutsplus.com/articles/envato-tuts-community-challenge-created-by-you-july-edition--cms-26724"
-                  className="card"
-                >
-                  <div
-                    className="thumb"
-                    href="https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/flex-1.jpg"
-                  />
-                  <article>
-                    <h1>Created by You, July Edition</h1>
-                    <p>
-                      Welcome to our monthly feature of fantastic tutorial results
-                      created by you, the Envato Tuts+ community!{" "}
-                    </p>
-                    <span>Melody Nieves</span>
-                  </article>
-                </a>
-              </div>
-              <div className="card-1">
-                <a
-                  href="https://webdesign.tutsplus.com/tutorials/how-to-code-a-scrolling-alien-lander-website--cms-26826"
-                  className="card"
-                >
-                  <div
-                    className="thumb"
-                    href="https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/flex-1.jpg"
-                  />
-                  <article>
-                    <h1>How to Code a Scrolling “Alien Lander” Website</h1>
-                    <p>
-                      We’ll be putting things together so that as you scroll down
-                      from the top of the page you’ll see an “Alien Lander” making
-                      its way to touch down.
-                    </p>
-                    <span>Kezz Bracey</span>
-                  </article>
-                </a>
-              </div>
-              <div className="card-1">
-                <a
-                  href="https://design.tutsplus.com/tutorials/stranger-things-inspired-text-effect--cms-27139"
-                  className="card"
-                >
-                  <div
-                    className="thumb"
-                    href="https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/flex-1.jpg"
-                  />
-                  <article>
-                    <h1>
-                      How to Create a “Stranger Things” Text Effect in Adobe
-                      Photoshop
-                    </h1>
-                    <span>Rose</span>
-                  </article>
-                </a>
-              </div>
-              <div className="card-1">
-                <a
-                  href="https://photography.tutsplus.com/articles/5-inspirational-business-portraits-and-how-to-make-your-own--cms-27338"
-                  className="card"
-                >
-                  <div
-                    className="thumb"
-                    href="https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/flex-1.jpg"
-                  />
-                  <article>
-                    <h1>
-                      5 Inspirational Business Portraits and How to Make Your Own
-                    </h1>
-                    <span>Marie Gardiner</span>
-                  </article>
-                </a>
-              </div>
-            </div>
+  state = {};
+  render() {
+    return (
+      <>
+        <div id="cardCont">
+              <div id="displayCards">
+                <ul className="card">
+                  <li className="card__image">
+                    <img src="https://nofilmschool.com/sites/default/files/styles/article_wide/public/bigstock.jpg?itok=Snv4I36d" alt="img"/>
+                  </li>
+                  <li className="card__content">
+                      <div className="card__title">Card Title</div>
+                    <b>
+                      <p className="card__text"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, tempore libero facere quos aspernatur assumenda. Impedit cupiditate, non aut saepe ratione sed dignissimos consequuntur? Dignissimos dicta sequi corporis enim asperiores. </p>{" "}
+                    </b>
+                  </li>
+                </ul>
          
-            </>   
-         );
-    }
+
+                <ul className="card">
+                  <li className="card__image">
+                    <img src="https://cdn.mos.cms.futurecdn.net/MbKXuF2uEgsWHX5c6MkCvY-320-80.jpg" alt="img"/>
+                  </li>
+                  <li className="card__content">
+                      <div className="card__title">Card Title</div>
+                    <b>
+                      <p className="card__text"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, tempore libero facere quos aspernatur assumenda. Impedit cupiditate, non aut saepe ratione sed dignissimos consequuntur? Dignissimos dicta sequi corporis enim asperiores. </p>{" "}
+                    </b>
+                  </li>
+                </ul>
+                <ul className="card">
+                  <li className="card__image">
+                    <img src="http://yeahstockphotos.com/wp-content/uploads/original/2016_05/free-stock-photos-high-resolution-download090.jpg" alt="img"/>
+                  </li>
+                  <li className="card__content">
+                      <div className="card__title">Card Title</div>
+                    <b>
+                      <p className="card__text"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, tempore libero facere quos aspernatur assumenda. Impedit cupiditate, non aut saepe ratione sed dignissimos consequuntur? Dignissimos dicta sequi corporis enim asperiores. </p>{" "}
+                    </b>
+                  </li>
+                </ul>
+                <ul className="card">
+                  <li className="card__image">
+                    <img src="https://codeshare.co.uk/media/1068/search-best-free-stock-images-one-place.jpg?width=1910&height=1000&mode=crop&anchor=center" alt="img"/>
+                  </li>
+                  <li className="card__content">
+                      <div className="card__title">Card Title</div>
+                    <b>
+                      <p className="card__text"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, tempore libero facere quos aspernatur assumenda. Impedit cupiditate, non aut saepe ratione sed dignissimos consequuntur? Dignissimos dicta sequi corporis enim asperiores. </p>{" "}
+                    </b>
+                  </li>
+                </ul>
+                </div>
+            </div>
+        
+    
+      </>
+    );
+  }
 }
- 
+
 export default HomePage;
